af0551e9e96730770befd9fbb1b4b53766d084a2
AK97Z3R0 <artkhv15@protonmail.com>
2019-10-04 00:02:48 -0500

    implemented AccountHolder structure

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+//accountHolder Struct
+type accountHolder struct {
+	accountNum   int
+	fName, lname string
+	accountBal   float64
+}
+
 func main() {
 	fmt.Println("What should be my project?")
 }
