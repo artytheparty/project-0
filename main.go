7417fb5a074ded107c52cf5fd35210654355a4e7
AK97Z3R0 <artkhv15@protonmail.com>
2019-10-07 22:54:09 -0500

    no changes Work on UI next

--- main.go
+++ main.go
@@ -9,16 +9,16 @@ import (
 
 func main() {
 	fmt.Println("Welcome to the Banking App")
-	admin := acc.CreateAccount("arty", "password", "arty", "sucess", 1, 5000)
-	admin2 := acc.CreateAccount("jjohna", "password", "john", "johna", 1, 5000)
-	admin3 := acc.CreateAccount("usernam", "password", "john", "johna", 1, 5000)
-	admin4 := acc.CreateAccount("shword", "password", "john", "johna", 1, 5000)
-	admin5 := acc.CreateAccount("uthough", "password", "john", "johna", 1, 5000)
-	admin6 := acc.CreateAccount("arty", "password", "arty", "sucess", 1, 5000)
-	admin7 := acc.CreateAccount("jjohna", "password", "john", "johna", 1, 5000)
-	admin8 := acc.CreateAccount("usernam", "password", "john", "johna", 1, 5000)
-	admin9 := acc.CreateAccount("shword", "password", "john", "johna", 1, 5000)
-	admin10 := acc.CreateAccount("uthough", "password", "john", "johna", 1, 5000)
+	admin := acc.CreateAccount("kjonah", "password", "Kyle", "Johna", 1, 5000)
+	admin2 := acc.CreateAccount("jjohna", "password", "John", "Johna", 2, 15000)
+	admin3 := acc.CreateAccount("ukra", "password", "Ursula", "Kung", 3, 5000)
+	admin4 := acc.CreateAccount("shword", "password", "Shelly", "Wordinson", 4, 5000)
+	admin5 := acc.CreateAccount("uthough", "password", "Umelia", "Thoughtful", 5, 5000)
+	admin6 := acc.CreateAccount("kps13", "password", "Kong", "Pierce", 6, 5000)
+	admin7 := acc.CreateAccount("bhide", "password", "Robert", "Hide", 7, 5000)
+	admin8 := acc.CreateAccount("dlecious", "password", "David", "Lecious", 8, 5000)
+	admin9 := acc.CreateAccount("hparker", "password", "Hong", "Parker", 9, 5000)
+	admin10 := acc.CreateAccount("golang", "password", "Godin", "Lang", 10, 5000)
 
 	//fmt.Println(admin.Info())
 	accounts := make(map[int]acc.AccountHolder)
@@ -33,10 +33,10 @@ func main() {
 	accounts[9] = admin9
 	accounts[10] = admin10
 
-	//dbhandling.WriteToFile(accounts)
-	accounts2 := dbhandling.ReadFile()
-	for k := range accounts2 {
-		accounts2[k].Info()
-	}
+	dbhandling.WriteToFile(accounts)
+	// accounts2 := dbhandling.ReadFile()
+	// for k := range accounts2 {
+	// 	accounts2[k].Info()
+	// }
 
 }
